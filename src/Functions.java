c107d9b2094a00d8e41ce4938c7ee26dfd598c6a
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-05-24 19:40:09 +0530

    Update Functions.java with power function

--- src/Functions.java
+++ src/Functions.java
@@ -16,25 +16,36 @@ public class Functions {
     //     System.out.println(factorial);
     // }
         
-    public static void checkPrime(int n){
-        if(n < 2){
-            System.out.println("Not Prime!");
-            return;
+    // public static void checkPrime(int n){
+    //     if(n < 2){
+    //         System.out.println("Not Prime!");
+    //         return;
+    //     }
+    //     for(int i = 2; i<= n-1; i++){
+    //         if(n % i == 0){
+    //             System.out.println("Not Prime!");
+    //             return;
+    //         }
+    //     }
+    //     System.out.println("Prime!");
+    // }
+
+   // Two numbers are entered by the user, x and n. Write a function to find the value 
+   // of one number raised to the power of another i.e. x.
+    public static void power(int x, int n){
+        int result = 1;
+        for(int i = 1; i <= n; i++){
+            result = result * x;
         }
-        for(int i = 2; i<= n-1; i++){
-            if(n % i == 0){
-                System.out.println("Not Prime!");
-                return;
-            }
-        }
-        System.out.println("Prime!");
-    }
+        System.out.println(result);
+    }   
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+        System.out.print("Enter the power: ");
         int n = sc.nextInt();
-        checkPrime(n);
-        
+        power(x, n);
     }
 }
