63f0ebd1b10db3a5d7741661e3869c930e5b4f98
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-05-24 20:37:24 +0530

    last update of 24 may

--- src/Functions.java
+++ src/Functions.java
@@ -86,9 +86,9 @@ public class Functions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int a = sc.nextInt();
+        
         sc.close();
 
-        
+
     }
 }
