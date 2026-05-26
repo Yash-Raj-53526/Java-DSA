b1d4634b3e00b11bc36dc1692a13426c183eef91
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-05-26 16:55:49 +0530

    refactor Strings class: update main method to focus on email username extraction

--- src/Strings.java
+++ src/Strings.java
@@ -1,10 +1,47 @@
+import java.util.*;
+
 public class Strings {
     public static void main(String[] args) {
-        String name = "Tony Stark";
-        // System.out.println(name.length());
+        // String name = "Tony Stark";
+        // // System.out.println(name.length());
 
-        for(int i = 0; i < name.length(); i++){
-            System.out.println(name.charAt(i)+ " at index -> " + i);
-        }
+        // for(int i = 0; i < name.length(); i++){
+        //     System.out.println(name.charAt(i)+ " at index -> " + i);
+        // }
+
+        //compareTo method is used to compare two strings lexicographically. It returns a positive integer if the first string is greater than the second string, a negative integer if the first string is less than the second string, and 0 if both strings are equal.
+        // String name1 = "Tony Stark";
+        // String name2 = "Tony Stark";
+        //s1 > s2 -> positive value
+        //s1 < s2 -> negative value
+        //s1 == s2 -> 0
+
+        // if(name1.compareTo(name2) == 0 ){
+        //     System.out.println("Strings are equal");
+        // } else {    
+        //     System.out.println("Strings are not equal");
+        // }
+
+        //equals() method is used to compare the content of two strings. It returns true if the content of both strings is the same, and false otherwise.
+        // if(name1.equals(name2)){
+        //     System.out.println("Strings are equal");
+        // } else {    
+        //     System.out.println("Strings are not equal");
+        // }
+
+        // String sentence = "my name is Yash Raj Pandey";
+        // //.substring(beginIndex, endIndex)
+        // String name = sentence.substring(11, 19);//endIndex is exclusive
+        // System.out.println(name);
+
+
+
+        //input email and print username 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your email: ");
+        String email = sc.next();
+        String username = email.substring(0, email.indexOf('@'));
+        System.out.println("Your username is: " + username);
+        sc.close();
     }
 }
