16cad8decc9663f39c9f5b1d696dfc40163b89c3
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-05-28 15:13:03 +0530

    array last

--- src/ARRAYS.java
+++ src/ARRAYS.java
@@ -70,28 +70,28 @@ public class ARRAYS {
 
 
     //2-d array
-        System.out.print("Enter the number of rows:");
-        int rows = sc.nextInt();
-        System.out.print("Enter the number of columns:");
-        int cols = sc.nextInt();
-        int[][] arr = new int[rows][cols];
-        System.out.println("Enter the elements of the 2D array:");
+        // System.out.print("Enter the number of rows:");
+        // int rows = sc.nextInt();
+        // System.out.print("Enter the number of columns:");
+        // int cols = sc.nextInt();
+        // int[][] arr = new int[rows][cols];
+        // System.out.println("Enter the elements of the 2D array:");
 
-        //input
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print("Element at (" + i + ", " + j + "): ");
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        //output
-        System.out.println("The elements of the 2D array are:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println(); // Move to the next line after each row
-        }
+        // //input
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         System.out.print("Element at (" + i + ", " + j + "): ");
+        //         arr[i][j] = sc.nextInt();
+        //     }
+        // }
+        // //output
+        // System.out.println("The elements of the 2D array are:");
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println(); // Move to the next line after each row
+        // }
 
 
         //print indices 
@@ -107,17 +107,82 @@ public class ARRAYS {
         // }
 
         //print data 
-        System.out.print("Enter the row of the element: ");
-        int r = sc.nextInt();
-        System.out.print("Enter the column of the element: ");
-        int c = sc.nextInt();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (r == i && c == j) {
-                    System.out.println("Element at index (" + r + ", " + c + ") is: " + arr[i][j]);
-                }
-            }
-        }
-    sc.close();
+        // System.out.print("Enter the row of the element: ");
+        // int r = sc.nextInt();
+        // System.out.print("Enter the column of the element: ");
+        // int c = sc.nextInt();
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         if (r == i && c == j) {
+        //             System.out.println("Element at index (" + r + ", " + c + ") is: " + arr[i][j]);
+        //         }
+        //     }
+        // }
+
+        //sum of all elements in the 2D array
+        // int sum = 0;
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         sum += arr[i][j];
+        //     }
+        // }
+        // System.out.println("Sum of all elements in the 2D array is: " + sum);
+
+
+        //sum of each row
+        // for (int i = 0; i < rows; i++) {
+        //     int rowSum = 0;
+        //     for (int j = 0; j < cols; j++) {
+        //         rowSum += arr[i][j]; 
+        //     }
+        //     System.out.println("Sum of row " + i + " is: " + rowSum);
+        // }
+
+
+        //product of all elements in the 2D array
+        // int product = 1;
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         product *= arr[i][j];
+        //     }
+        // }
+        // System.out.println("Product of all elements in the 2D array is: " + product);
+
+
+        //product of each column
+        // for (int j = 0; j < cols; j++) {
+        //     int colProduct = 1;
+        //     for (int i = 0; i < rows; i++) {
+        //         colProduct *= arr[i][j];
+        //     }
+        //     System.out.println("Product of column " + j + " is: " + colProduct);
+        // }    
+
+        //product of each row
+        // for (int i = 0; i < rows; i++) {
+        //     int rowProduct = 1;
+        //     for (int j = 0; j < cols; j++) {
+        //         rowProduct *= arr[i][j];
+        //     }
+        //     System.out.println("Product of row " + i + " is: " + rowProduct);
+        // }
+
+        //find max and min in 2D array
+        // int max = arr[0][0];
+        // int min = arr[0][0];
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         if (arr[i][j] > max) {
+        //             max = arr[i][j];
+        //         }
+        //         if (arr[i][j] < min) {
+        //             min = arr[i][j];
+        //         }    
+        //     }
+        // }
+        // System.out.println("The maximum element in the 2D array is: " + max);
+        // System.out.println("The minimum element in the 2D array is: " + min);
+
+        sc.close();
     }
 }
