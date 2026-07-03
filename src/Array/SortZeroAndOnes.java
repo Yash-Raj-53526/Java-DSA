ad112bd124983edf064b4c7e1265d35ca7087337
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-07-03 11:39:42 +0530

    feat: add two-pointer implementation for sorting binary array

--- src/Array/SortZeroAndOnes.java
+++ src/Array/SortZeroAndOnes.java
@@ -1,23 +1,22 @@
 
 package Array;
 
-public class SortZeroAndOnes{
-    //two pointer approach
-    public static int[] sort(int[] arr){
+public class SortZeroAndOnes {
+    // two pointer approach
+    public static int[] sort(int[] arr) {
         int i = 0;
-        int j = arr.length -1;
-        while(i<j){
-            if(arr[i]==1 && arr[j]==0){
-                //swap
+        int j = arr.length - 1;
+        while (i < j) {
+            if (arr[i] == 1 && arr[j] == 0) {
+                // swap
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
                 i++;
                 j--;
-            }
-            else if(arr[i]==0){
+            } else if (arr[i] == 0) {
                 i++;
-            }else if(arr[j]==1){
+            } else if (arr[j] == 1) {
                 j--;
             }
         }
@@ -25,10 +24,10 @@ public class SortZeroAndOnes{
     }
 
     public static void main(String[] args) {
-        int arr[] = {0,1,0,1,1,0,0,1,1,1,0,0,0,1};
+        int arr[] = { 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1 };
         int ans[] = sort(arr);
-        for(int i=0; i<ans.length; i++){
+        for (int i = 0; i < ans.length; i++) {
             System.out.print(ans[i] + " ");
         }
-    }  
+    }
 }
