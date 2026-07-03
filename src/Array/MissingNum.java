47fa8b6c7ef34ffbf806274f4d4ebc1cba512b12
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-07-03 12:27:55 +0530

    update

--- src/Array/MissingNum.java
+++ src/Array/MissingNum.java
@@ -1,12 +1,22 @@
 package Array;
 
+import java.util.Arrays;
+
 public class MissingNum {
-    public int MissingNum(int[] nums) {
+    public static int missingNum(int[] nums) {
         int xorSum = 0;
         for (int n : nums) {
             xorSum = xorSum ^ n;
         }
-
+        int n = nums.length;
+        for (int i = 0; i <= n; i++) {
+            xorSum = xorSum ^ i;
+        }
+        return xorSum;
     }
 
+    public static void main(String[] args) {
+        int[] nums = { 3, 0, 1 };
+        System.out.println("Missing number in " + Arrays.toString(nums) + " is: " + missingNum(nums));
+    }
 }
