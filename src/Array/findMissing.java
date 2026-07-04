3a89f43fae754e1eed975023bf6e2c9420021d7f
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-07-04 16:02:51 +0530

    Find Missing Num

---
+++ src/Array/findMissing.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.*;
+
+public class findMissing {
+    public static List<Integer> find(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int val = Math.abs(arr[i]);
+            if (arr[val - 1] > 0)
+                arr[val - 1] *= -1;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                ans.add(i + 1);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 5, 5, 4 };
+        System.out.println(find(arr));
+    }
+}
