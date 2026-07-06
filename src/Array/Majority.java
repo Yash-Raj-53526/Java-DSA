9a9f41ee0f50eddb528765f40642c24086479820
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-07-06 12:12:31 +0530

    majority element

---
+++ src/Array/Majority.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.HashMap;
+
+public class Majority {
+    public static int majority(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (map.get(arr[i]) > arr.length / 2) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 2, 1, 1, 1, 2, 2 };
+        System.out.println(majority(arr));
+    }
+
+}
