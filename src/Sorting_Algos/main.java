ff7dac3d23dfe46b27536baab2e55bd9c5e7ae34
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-07-08 22:00:13 +0530

    feat: implement bubble, selection, and insertion sort algorithms in main class

--- src/Sorting_Algos/main.java
+++ src/Sorting_Algos/main.java
@@ -1,7 +1,7 @@
 package Sorting_Algos;
 
 public class main {
-    public static void BubbleSort(int arr[]) {
+    public static void bubbleSort(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
@@ -15,12 +15,46 @@ public class main {
         }
     }
 
-    public static void main(String[] args) {
-        int arr[] = { 1, 4, 3, 2, 5 };
-        BubbleSort(arr);
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
+        }
+    }
+
+    public static void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+    }
+
+    public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+    }
 
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 3, 2, 5 };
+        insertionSort(arr);
+        printArray(arr);
     }
 }
