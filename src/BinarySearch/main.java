21a7bbfe04004f45c8286dcfefa603dc82a22007
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-07-09 17:37:19 +0530

    feat: implement binary search algorithm in main class

---
+++ src/BinarySearch/main.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+public class main {
+    public static int binarySearch(int arr[], int target) {
+        int n = arr.length;
+        int start = 0;
+        int end = n - 1;
+        int mid = (start + end) / 2;
+        while (start <= end) {
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            mid = (start + end) / 2;
+        }
+        return -1;
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 3, 5, 1, 4 };
+        binarySearch(arr, 3);
+        // printArray(arr);
+    }
+}
