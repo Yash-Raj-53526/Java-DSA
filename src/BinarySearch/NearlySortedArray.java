a20a10960d71c1dc18939a17c5b2ef6c999fa5db
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-07-14 20:30:29 +0530

    feat: implement binary search algorithm for nearly sorted arrays

---
+++ src/BinarySearch/NearlySortedArray.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+public class NearlySortedArray {
+    public static int search(int arr[], int tar) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == tar) {
+                return mid;
+            }
+            if (arr[mid - 1] >= start && arr[mid - 1] == tar) {
+                return mid - 1;
+            }
+            if (arr[mid + 1] <= end && arr[mid + 1] == tar) {
+                return mid + 1;
+            }
+            if (arr[mid] > tar) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 3, 40, 20, 50, 80, 70 };
+        int n = arr.length;
+        int target = 40;
+        System.out.println(search(arr, target));
+    }
+
+}
+
+// time complexity O(logN)
+// space complexity O(1)
